165c114602e636717d8b837940f73b2b2cefaf16
bsbiradar <bbiradar.com>
2024-07-31 18:09:15 +0530

    added password conf

--- AWS-IAM/main.tf
+++ AWS-IAM/main.tf
@@ -30,7 +30,19 @@ resource "aws_iam_user" "cli_users" {
     name = each.value
 
 }
+resource "aws_iam_user_login_profile" "profile" {
+  for_each        = aws_iam_user.cli_users
+  user            = each.value.name
+  password_length = 12
 
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
+}
 
 resource "aws_iam_user_policy_attachment" "main" {
   for_each = {
